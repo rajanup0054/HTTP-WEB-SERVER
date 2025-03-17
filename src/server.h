b6522f87c2c38e1541ab23b6ac7e627964f6da8d
rajanup0054 <rajanup0054@gmail.com>
2025-03-17 11:39:03 +0530

    First

---
+++ src/server.h
@@ -0,0 +1,15 @@
+#ifndef SERVER_H
+#define SERVER_H
+#include<iostream>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<unistd.h>
+#include<errno.h>
+#include<string>
+#include<fcntl.h>
+#include<sys/epoll.h>
+#include<assert.h>
+#include<signal.h>
+#include<sys/wait.h>
+#endif
